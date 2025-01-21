707c111fd37b66081d58bb4a0ef9ad4638ba9cc7
Danse89 <richard.cheese89@googlemail.com>
2025-01-21 11:41:02 +0100

    Add files via upload

---
+++ src/main/java/com/example/pruefungsrechner/PruefungsrechnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pruefungsrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruefungsrechnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruefungsrechnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pruefungsrechner/PruefungsrechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pruefungsrechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruefungsrechnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
