940398f82cba29f7dbef3453baf251cd56279ac5
mathias <mathias.kartmann@aol.de>
2025-01-30 13:16:39 +0100

    Anwendung Läuft

---
+++ src/main/java/services/MvcConfig.java
@@ -0,0 +1,38 @@
+package services;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LocaleChangeInterceptor localeChangeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor); // Verwende die Bean statt einer neuen Instanz
+    }
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/Chatseite").setViewName("chat");
+//        registry.addViewController("/").setViewName("login");
+//        registry.addViewController("/Login").setViewName("login");
+       // registry.addViewController("/registration").setViewName("registration");
+       // registry.addViewController("/kalendar").setViewName("kalendar");
+        //registry.addViewController("/lernplan").setViewName("lernplan");
+       // registry.addViewController("/projekt").setViewName("projekt");
+       // registry.addViewController("/user").setViewName("user");
+       // registry.addViewController("/uebersicht").setViewName("uebersicht");
+       // registry.addViewController("/password").setViewName("password");
+       // registry.addViewController("/password-reset").setViewName("password-reset");
+
+    }
+
+}
