41a1b96013cbb6e01befcb9a2609b76f281f0878
Danse89 <richard.cheese89@googlemail.com>
2025-02-12 11:49:23 +0100

    WetterAPI läuft

--- src/main/java/com/example/pruefungsrechner/Config/AppConfig.java
+++ src/main/java/com/example/pruefungsrechner/Config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.example.pruefungsrechner.config;
+package com.example.pruefungsrechner.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
